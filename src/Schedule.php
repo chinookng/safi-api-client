f2f2d6c626ba5f737bced66b794f3d9f07b4a34e
Laju Morrison <morrelinko@gmail.com>
2016-09-01 10:40:37 +0100

    use live data for daily schedules

--- src/Schedule.php
+++ src/Schedule.php
@@ -28,54 +28,6 @@ class Schedule
 
     public function dailySchedules()
     {
-        $res = [];
-        $times = ['08', '10', '12', '14', '16', '18', '20'];
-
-        foreach ($times as $time) {
-            $res[] = [
-                'time' => $time . ':00:00 - ' . ($time + 2) . ':00:00',
-                'pickups' => 32
-            ];
-        }
-
-        return $res;
-    }
-
-    protected function generateMockSchedules()
-    {
-        $schedules = [];
-        $period = Period::createFromMonth(2016, 6);
-
-        foreach ($period->getDatePeriod('1 DAY') as $day) {
-            /** @var $day \DateTimeImmutable */
-            $schedules[$day->format('Y-m-d')] = $this->generateMockTimes();
-            $schedules[$day->format('Y-m-d')] = $this->generateMockTimes();
-        }
-
-        return $schedules;
-    }
-
-    protected function generateMockTimes()
-    {
-        $res = [];
-        $times = ['08', '10', '12', '14', '16', '18', '20'];
-
-        foreach ($times as $time) {
-            $res[$time . ':00:00 - ' . ($time + 2) . ':00:00'] = [
-                'collections' => [
-                    'is_available' => true,
-                    'total_pickup' => 12,
-                    'time_size' => 32
-                ],
-                'deliveries' => [
-                    'time' => $time . ':00:00 - ' . ($time + 2) . ':00:00',
-                    'is_available' => true,
-                    'total_pickup' => 12,
-                    'time_size' => 32
-                ]
-            ];
-        }
-
-        return $res;
+        return $this->client->call('GET', '');
     }
 }
